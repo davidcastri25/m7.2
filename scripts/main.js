6ce2aeeb03a9808960641c8d317e8afc0c4be4bc
David Castrillón <david.castri@hotmail.es>
2021-06-08 17:09:38 +0200

    08/06/2021 - Completados los 5 ejercicios, pendiente de entender el proceso de map

--- scripts/main.js
+++ scripts/main.js
@@ -53,21 +53,65 @@ for (let i = 0; i < nomEx2.length; i++) {
 
 
 /* 3- Emmagatzemar en array les lletres i nombre de vegades que apareixen. Utilitza el mètode map().*/
-let nomEx3 = ["D", "A", "V", "I", "D", "D", "D", "A", "V", "V"];
+let nomEx3 = ["D", "A", "V", "I", "D"];
 
-let nomMap = nomEx3.map(function(letra){
-    //Creamos un array con todas las repeticiones de una letra
-    let nomFilter = nomEx3.filter(function(caracter){
-        return caracter == letra;
-    });
+console.log("Partiendo del array: " + nomEx3);
 
-    return nomFilter[0] + ": " + nomFilter.length;
-
+var nomMap = {};
+nomEx3.map(function(letra){
+    //Crearemos una propiedad, el nombre de la cual será la letra que estamos iterando con map en este momento
+    nomMap[letra] = (nomMap[letra] || 0) + 1;
+    //Si la propiedad no existe, la creará y le dará valor 0, luego le sumará uno. Si la propiedad ya existe, le sumará 1 al valor que ya tenía
+    
 });
 
 console.log(nomMap);
 
-let nomMap2 = nomEx3.map(function(letra){
+/* Esto sería lo mismo pero con forEach
+let nomMap = contarLetras(nomEx3);
+
+console.log(nomMap);
+
+function contarLetras (arrLetras){
+    let letrasContadas = {};
+
+    arrLetras.forEach(function (letra){
+        if (letra in letrasContadas) {
+            letrasContadas[letra]++;
+        }
+        else {
+            letrasContadas[letra] = 1;
+        }
+    });
+
+    return letrasContadas;
+} */
+
+//let nomMap = nomEx3.map(function(letra){
+   // let elementReturn;    
+
+    //Creamos un array con todas las repeticiones de la letra en la que se situa la iteración
+    //let nomFilter = nomEx3.filter(function(caracter){
+        //return caracter == letra;
+    //});
+
+    //Devolvemos cualquier elemento del array que se acaba de crear más su length, que será el número de repeticiones de la letra
+    //elementReturn = nomFilter[0] + ": " + nomFilter.length;  
+
+    //return elementReturn;
+    
+//});
+
+
+
+/*function removeDuplicates(array) {
+    let mySet = new Set (array);
+    return Array.from(mySet);
+}
+
+console.log(removeDuplicates(nomMap));  */
+
+/*let nomMap2 = nomEx3.map(function(letra){
     let numRepeticiones;
     let contador = 0;
     
@@ -82,7 +126,7 @@ let nomMap2 = nomEx3.map(function(letra){
     return numRepeticiones;    
 });
 
-console.log(nomMap2);
+console.log(removeDuplicates(nomMap2));  */
 
 
 /* 4-
@@ -99,10 +143,17 @@ console.log(arrFullName);
 /* Nivel 2- Crea una funció que retorni un array amb tots els email (sense repetir) del següent text:*/
 var str = 'Una dirección de correo electrónico es la dirección que utiliza para recibir y enviar correos electrónicos. Se muestra a los destinatarios de sus correos electrónicos para que sepan quién le envió un correo electrónico. Cada dirección de correo electrónico sólo se puede asignar una vez en todo el mundo y, por lo tanto, está disponible exclusivamente para usted. No puede cambiar las direcciones de correo electrónico, pero puede eliminarlas en cualquier momento. Una dirección de correo electrónico consiste en el signo @ (arroba), el nombre de usuario delante y el dominio detrás, por ejemplo, nombre-de-usuario@ionos.es: La parte del dominio depende del dominio bajo el cual se crea la dirección de correo electrónico: en nuestro ejemplo es ionos.es. Esta información varía de proveedor a proveedor, por lo que una parte del dominio también puede ser gmail.com o gmx.es si utiliza una dirección de correo electrónico de estos proveedores. Si ha registrado su propio dominio, por ejemplo, www.el-nombre-de-sus-sueños.es, las direcciones de correo electrónico que configura para el dominio lo tienen como parte del dominio (nombre-de-usuario@el-nombre-de-sus-sueños.es o nombre-de-usuario@el-nombre-de-sus-sueños.ES). El nombre de usuario es la parte de una dirección de correo electrónico que puede seleccionar libremente en la medida de lo posible. Puede, por ejemplo, utilizar su propio nombre o el nombre o departamento de una empresa. Si utiliza una dirección de correo electrónico con un proveedor de correo como gmail.com o gmx.es, es posible que la combinación con la parte del dominio deseada ya esté registrada. En este caso, deberá considerar alternativas para el nombre de usuario de su dirección de correo electrónico. Si utiliza su propio dominio, estas restricciones no se aplican porque sólo usted puede crear direcciones de correo electrónico que coincidan con su propio dominio. En resumen, nombre-de-usuario@ionos.es es un email'; 
 
-console.log(returnArrEmail(str));
+let arrValidaciones = returnArrEmail(str)
+
+console.log(removeDuplicates(arrValidaciones));
 
 function returnArrEmail (texto) {    
     const expresion =/([a-z0-9-]+)@([a-z0-9-ñ]+)\.([a-z]{2,8})(\.[a-z]{2,8})?/gi;
     let validation = texto.match(expresion);
     return validation;
+}
+
+function removeDuplicates(array) {
+    let mySet = new Set (array);
+    return Array.from(mySet);
 }
