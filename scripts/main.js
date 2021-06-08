101e7213469d7a337c2f5e629e58d16d4ef20345
David Castrillón <david.castri@hotmail.es>
2021-06-08 13:46:55 +0200

    08/06/2021 - Avanzo ejercicios, pendiente solucionar map y solucionar la repetición del array de validación

--- scripts/main.js
+++ scripts/main.js
@@ -54,27 +54,55 @@ for (let i = 0; i < nomEx2.length; i++) {
 
 /* 3- Emmagatzemar en array les lletres i nombre de vegades que apareixen. Utilitza el mètode map().*/
 let nomEx3 = ["D", "A", "V", "I", "D", "D", "D", "A", "V", "V"];
-let nuevoEx3 = [];
-let count = 0;
 
-for (let i = 0; i < nomEx3.length; i++) {
-    for (let j = i + 1; j < nomEx3.length; j++) {
-        if (nomEx3[i] == nomEx3[j]) {
-            count++;
-            nomEx3.splice(j, 1);
+let nomMap = nomEx3.map(function(letra){
+    //Creamos un array con todas las repeticiones de una letra
+    let nomFilter = nomEx3.filter(function(caracter){
+        return caracter == letra;
+    });
+
+    return nomFilter[0] + ": " + nomFilter.length;
+
+});
+
+console.log(nomMap);
+
+let nomMap2 = nomEx3.map(function(letra){
+    let numRepeticiones;
+    let contador = 0;
+    
+    for (let i = 0; i < nomEx3.length; i++) {
+        if (letra == nomEx3[i]) {
+            contador++;
         }
     }
 
-    nuevoEx3.push(nomEx3[i] + ": " + count);
+    numRepeticiones = letra + ": " + contador;
 
-    count = 0;
+    return numRepeticiones;    
+});
+
+console.log(nomMap2);
+
+
+/* 4-
+- Crea una altra array amb el teu cognom on cada posició correspongui a una lletra.
+- Fusiona els dos arrays en un. A més, afegeix una posició amb un espai buit entre la primera i la segona. És a dir, partim de 'array name i surname i al acabar l’execució només tindrem una que es dirà fullName.*/
+
+let arrName = ["D", "A", "V", "I", "D"];
+let arrSurname = ["C", "A", "S", "T", "R", "I", "L", "L", "Ó", "N"];
+let arrFullName = arrName.concat(" ").concat(arrSurname);
+
+console.log(arrFullName);
+
+
+/* Nivel 2- Crea una funció que retorni un array amb tots els email (sense repetir) del següent text:*/
+var str = 'Una dirección de correo electrónico es la dirección que utiliza para recibir y enviar correos electrónicos. Se muestra a los destinatarios de sus correos electrónicos para que sepan quién le envió un correo electrónico. Cada dirección de correo electrónico sólo se puede asignar una vez en todo el mundo y, por lo tanto, está disponible exclusivamente para usted. No puede cambiar las direcciones de correo electrónico, pero puede eliminarlas en cualquier momento. Una dirección de correo electrónico consiste en el signo @ (arroba), el nombre de usuario delante y el dominio detrás, por ejemplo, nombre-de-usuario@ionos.es: La parte del dominio depende del dominio bajo el cual se crea la dirección de correo electrónico: en nuestro ejemplo es ionos.es. Esta información varía de proveedor a proveedor, por lo que una parte del dominio también puede ser gmail.com o gmx.es si utiliza una dirección de correo electrónico de estos proveedores. Si ha registrado su propio dominio, por ejemplo, www.el-nombre-de-sus-sueños.es, las direcciones de correo electrónico que configura para el dominio lo tienen como parte del dominio (nombre-de-usuario@el-nombre-de-sus-sueños.es o nombre-de-usuario@el-nombre-de-sus-sueños.ES). El nombre de usuario es la parte de una dirección de correo electrónico que puede seleccionar libremente en la medida de lo posible. Puede, por ejemplo, utilizar su propio nombre o el nombre o departamento de una empresa. Si utiliza una dirección de correo electrónico con un proveedor de correo como gmail.com o gmx.es, es posible que la combinación con la parte del dominio deseada ya esté registrada. En este caso, deberá considerar alternativas para el nombre de usuario de su dirección de correo electrónico. Si utiliza su propio dominio, estas restricciones no se aplican porque sólo usted puede crear direcciones de correo electrónico que coincidan con su propio dominio. En resumen, nombre-de-usuario@ionos.es es un email'; 
+
+console.log(returnArrEmail(str));
+
+function returnArrEmail (texto) {    
+    const expresion =/([a-z0-9-]+)@([a-z0-9-ñ]+)\.([a-z]{2,8})(\.[a-z]{2,8})?/gi;
+    let validation = texto.match(expresion);
+    return validation;
 }
-
-console.log(nuevoEx3);
-
-/*let nomMap = nomEx3.map(function(letra, i){
-    
-    if (letra[i] ==)
-})
-
-console.log(nomMap);  */
