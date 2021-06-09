7169a9060c4a2073365f0d19e9572b4361753af9
David Castrillón <david.castri@hotmail.es>
2021-06-09 15:39:44 +0200

    09/06/2021 - Agrego a comentario última explicación sobre || en map, declaro arrays como const como buena práctica, adapto dos funciones a arrow functions

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@
 - Crea un array amb el teu nom on cada posició correspongui a una lletra.
 - Fes un bucle que recorri aquest array i mostri per consola cadascuna de les lletres. */
 
-let nom = ["D", "A", "V", "I", "D"];
+const nom = ["D", "A", "V", "I", "D"];
 
 for (let i = 0; i < nom.length; i++) {
     console.log(nom[i]);
@@ -13,7 +13,7 @@ for (let i = 0; i < nom.length; i++) {
 - Al bucle, si la lletra és una vocal imprimeix a la consola: ‘He trobat la VOCAL: __’. Sinó, imprimeix: ‘He trobat la CONSONTANT: __’.
 - Si trobes un número, mostra per pantalla: ‘Els noms de persones no contenen el número: __’ */
 
-let nomEx2 = ["D", "A", "V", "I", 1, "D"];
+const nomEx2 = ["D", "A", "V", "I", 1, "D"];
 let mensaje = "Partim del nom: ";
 
 for (let i = 0; i < nomEx2.length; i++) {
@@ -53,16 +53,16 @@ for (let i = 0; i < nomEx2.length; i++) {
 
 
 /* 3- Emmagatzemar en array les lletres i nombre de vegades que apareixen. Utilitza el mètode map().*/
-let nomEx3 = ["D", "A", "V", "I", "D"];
+const nomEx3 = ["D", "A", "V", "I", "D"];
 
 console.log("Partiendo del array: " + nomEx3);
 
 var nomMap = {};
-nomEx3.map(function(letra){    
-    //Crearemos una propiedad, el nombre de la cual será la letra que estamos iterando con map en este momento
+nomEx3.map(letra => {    
+    //Crearemos una propiedad en el objeto nomMap, el nombre de la cual será la letra que estamos iterando con map en este momento
     nomMap[letra] = (nomMap[letra] || 0) + 1;
-    //Si la propiedad no existe, la creará y le dará valor 0, luego le sumará uno. Si la propiedad ya existe, le sumará 1 al valor que ya tenía
-    //Similar al concepto optional chaining    
+    //Si la propiedad no existe, la creará y le dará valor 0, luego le sumará 1. Si la propiedad ya existe, le sumará 1 al valor que ya tenía
+    //El operador || hace de short circuit operator. Si el primer operando da true (se ha encontrado un valor para nomMap[letra]), entonces el segundo operando se saltará y le sumaremos 1 al valor que se ha encontrado. Si el primer operando da false (no se ha encontrado un valor para nomMap[letra] y por lo tanto, esa letra no existe en nomMap), entonces evaluaremos el segundo operando (que es 0, y por tanto generaremos un elemento nomMap[letra] = 0 y le sumaremos 1).   
 });
 
 console.log(nomMap);
@@ -121,9 +121,9 @@ console.log(removeDuplicates(nomMap));  */
 - Crea una altra array amb el teu cognom on cada posició correspongui a una lletra.
 - Fusiona els dos arrays en un. A més, afegeix una posició amb un espai buit entre la primera i la segona. És a dir, partim de 'array name i surname i al acabar l’execució només tindrem una que es dirà fullName.*/
 
-let arrName = ["D", "A", "V", "I", "D"];
-let arrSurname = ["C", "A", "S", "T", "R", "I", "L", "L", "Ó", "N"];
-let arrFullName = arrName.concat(" ").concat(arrSurname);
+const arrName = ["D", "A", "V", "I", "D"];
+const arrSurname = ["C", "A", "S", "T", "R", "I", "L", "L", "Ó", "N"];
+const arrFullName = arrName.concat(" ").concat(arrSurname);
 
 console.log(arrFullName);
 
@@ -131,15 +131,15 @@ console.log(arrFullName);
 /* Nivel 2- Crea una funció que retorni un array amb tots els email (sense repetir) del següent text:*/
 var str = 'Una dirección de correo electrónico es la dirección que utiliza para recibir y enviar correos electrónicos. Se muestra a los destinatarios de sus correos electrónicos para que sepan quién le envió un correo electrónico. Cada dirección de correo electrónico sólo se puede asignar una vez en todo el mundo y, por lo tanto, está disponible exclusivamente para usted. No puede cambiar las direcciones de correo electrónico, pero puede eliminarlas en cualquier momento. Una dirección de correo electrónico consiste en el signo @ (arroba), el nombre de usuario delante y el dominio detrás, por ejemplo, nombre-de-usuario@ionos.es: La parte del dominio depende del dominio bajo el cual se crea la dirección de correo electrónico: en nuestro ejemplo es ionos.es. Esta información varía de proveedor a proveedor, por lo que una parte del dominio también puede ser gmail.com o gmx.es si utiliza una dirección de correo electrónico de estos proveedores. Si ha registrado su propio dominio, por ejemplo, www.el-nombre-de-sus-sueños.es, las direcciones de correo electrónico que configura para el dominio lo tienen como parte del dominio (nombre-de-usuario@el-nombre-de-sus-sueños.es o nombre-de-usuario@el-nombre-de-sus-sueños.ES). El nombre de usuario es la parte de una dirección de correo electrónico que puede seleccionar libremente en la medida de lo posible. Puede, por ejemplo, utilizar su propio nombre o el nombre o departamento de una empresa. Si utiliza una dirección de correo electrónico con un proveedor de correo como gmail.com o gmx.es, es posible que la combinación con la parte del dominio deseada ya esté registrada. En este caso, deberá considerar alternativas para el nombre de usuario de su dirección de correo electrónico. Si utiliza su propio dominio, estas restricciones no se aplican porque sólo usted puede crear direcciones de correo electrónico que coincidan con su propio dominio. En resumen, nombre-de-usuario@ionos.es es un email'; 
 
-var arrValidaciones = returnArrEmail(str)
+const arrValidaciones = returnArrEmail(str)
 
 function returnArrEmail (texto) {    
     const expresion =/([a-z0-9-]+)@([a-z0-9-ñ]+)\.([a-z]{2,8})(\.[a-z]{2,8})?/gi;
-    let validation = texto.match(expresion);
+    const validation = texto.match(expresion);
     return validation;
 }
 
-const arrSinDuplicados = arrValidaciones.filter(function(value, index){
+const arrSinDuplicados = arrValidaciones.filter((value, index) => {
     return arrValidaciones.indexOf(value) === index;
     //El método indexOf retorna el índice de la PRIMERA ocurrencia de un elemento en un array
     //Si el índice del elemento que estamos iterando con filter es igual al índice de la primera ocurrencia de ese elemento (arrValidaciones.indexOf(value)), se cumplirá la condición de filter para que nos retorne el valor en el nuevo array que se está generando. Si el índice del elemento que estamos iterando con filter es diferente al índice de la primera ocurrencia de ese elemento(arrValidaciones.indexOf(value)), significa que es una repetición.
