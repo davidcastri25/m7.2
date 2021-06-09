a0296b5c7bca9dd892f1a41eb50e24e37f2dee15
David Castrillón <david.castri@hotmail.es>
2021-06-09 10:01:58 +0200

    09/06/2021 - Busco otra forma de eliminar duplicados en el ejercicio de nivel 2, con filter

--- scripts/main.js
+++ scripts/main.js
@@ -87,6 +87,7 @@ function contarLetras (arrLetras){
     return letrasContadas;
 } */
 
+//Una tercera forma de hacerlo, en la que se necesita una forma para eliminar los duplicados del array que nos genera map. En este ejemplo el resultado del array no serán propiedades y valores, si no una string generada por concatenación.
 //let nomMap = nomEx3.map(function(letra){
    // let elementReturn;    
 
@@ -102,31 +103,12 @@ function contarLetras (arrLetras){
     
 //});
 
-
-
 /*function removeDuplicates(array) {
     let mySet = new Set (array);
     return Array.from(mySet);
 }
 
 console.log(removeDuplicates(nomMap));  */
-
-/*let nomMap2 = nomEx3.map(function(letra){
-    let numRepeticiones;
-    let contador = 0;
-    
-    for (let i = 0; i < nomEx3.length; i++) {
-        if (letra == nomEx3[i]) {
-            contador++;
-        }
-    }
-
-    numRepeticiones = letra + ": " + contador;
-
-    return numRepeticiones;    
-});
-
-console.log(removeDuplicates(nomMap2));  */
 
 
 /* 4-
@@ -143,9 +125,7 @@ console.log(arrFullName);
 /* Nivel 2- Crea una funció que retorni un array amb tots els email (sense repetir) del següent text:*/
 var str = 'Una dirección de correo electrónico es la dirección que utiliza para recibir y enviar correos electrónicos. Se muestra a los destinatarios de sus correos electrónicos para que sepan quién le envió un correo electrónico. Cada dirección de correo electrónico sólo se puede asignar una vez en todo el mundo y, por lo tanto, está disponible exclusivamente para usted. No puede cambiar las direcciones de correo electrónico, pero puede eliminarlas en cualquier momento. Una dirección de correo electrónico consiste en el signo @ (arroba), el nombre de usuario delante y el dominio detrás, por ejemplo, nombre-de-usuario@ionos.es: La parte del dominio depende del dominio bajo el cual se crea la dirección de correo electrónico: en nuestro ejemplo es ionos.es. Esta información varía de proveedor a proveedor, por lo que una parte del dominio también puede ser gmail.com o gmx.es si utiliza una dirección de correo electrónico de estos proveedores. Si ha registrado su propio dominio, por ejemplo, www.el-nombre-de-sus-sueños.es, las direcciones de correo electrónico que configura para el dominio lo tienen como parte del dominio (nombre-de-usuario@el-nombre-de-sus-sueños.es o nombre-de-usuario@el-nombre-de-sus-sueños.ES). El nombre de usuario es la parte de una dirección de correo electrónico que puede seleccionar libremente en la medida de lo posible. Puede, por ejemplo, utilizar su propio nombre o el nombre o departamento de una empresa. Si utiliza una dirección de correo electrónico con un proveedor de correo como gmail.com o gmx.es, es posible que la combinación con la parte del dominio deseada ya esté registrada. En este caso, deberá considerar alternativas para el nombre de usuario de su dirección de correo electrónico. Si utiliza su propio dominio, estas restricciones no se aplican porque sólo usted puede crear direcciones de correo electrónico que coincidan con su propio dominio. En resumen, nombre-de-usuario@ionos.es es un email'; 
 
-let arrValidaciones = returnArrEmail(str)
-
-console.log(removeDuplicates(arrValidaciones));
+var arrValidaciones = returnArrEmail(str)
 
 function returnArrEmail (texto) {    
     const expresion =/([a-z0-9-]+)@([a-z0-9-ñ]+)\.([a-z]{2,8})(\.[a-z]{2,8})?/gi;
@@ -153,7 +133,17 @@ function returnArrEmail (texto) {
     return validation;
 }
 
-function removeDuplicates(array) {
-    let mySet = new Set (array);
+const arrSinDuplicados = arrValidaciones.filter(function(value, index){
+    return arrValidaciones.indexOf(value) === index;
+    //El método indexOf retorna el índice de la PRIMERA ocurrencia de un elemento en un array
+    //Si el índice del elemento que estamos iterando con filter es igual al índice de la primera ocurrencia de ese elemento (arrValidaciones.indexOf(value)), se cumplirá la condición de filter para que nos retorne el valor en el nuevo array que se está generando. Si el índice del elemento que estamos iterando con filter es diferente al índice de la primera ocurrencia de ese elemento(arrValidaciones.indexOf(value)), significa que es una repetición.
+});
+
+console.log(arrSinDuplicados);
+
+//Una forma que funciona para eliminar duplicados de una array
+/* function removeDuplicates(arr) {
+    let mySet = new Set (arr);
     return Array.from(mySet);
-}
+} */
+// console.log(removeDuplicates(arrValidaciones));
